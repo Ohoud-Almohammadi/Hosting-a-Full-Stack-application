d8fa5e7daf92f7b3231208b8d50cf2db9ac7bd65
Ohoud Almohammadi <ohoud.hrb@gmail.com>
2022-12-31 16:15:45 +0300

    first push

---
+++ udagram/set_env.sh
@@ -0,0 +1,13 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_HOST=database-1.chi0rp99krui.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::mybucket495892423351
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=letsdoit
+export URL=http://localhost:8100
+export AWS_ACCESS_KEY_ID=AKIAXG5L54K33ZVKZXUK
+export AWS_SECRET_ACCESS_KEY=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
